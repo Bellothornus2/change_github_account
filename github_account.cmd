b0917040ad59e8180cbe3cccb0d53f80641e67a1
bellothornus2 <divanov@cifpfbmoll.eu>
2020-11-14 01:15:40 +0100

    small refactoring

--- github_account.cmd
+++ github_account.cmd
@@ -1,23 +1,27 @@
 @echo off
-
-
-set /p account=Elige Personal o Educativa (P o E):
+echo tu cuenta es:
+echo usuario:
+git config user.name
+echo email: 
+git config user.email
+set /p account=Elige Personal o Educativa o salir (P o E o exit):
 
 if %account%==P goto :personal 
 if %account%==E goto :educativa
+if %account%==exit goto :final
 
 :educativa
 git config --unset credential.helper
 git config --global user.name bellothornus2
 git config --global user.email divanov@cifpfbmoll.eu
-goto :final2
+goto :final
 
 :personal
 git config --unset credential.helper
 git config --global user.name bellothornus
 git config --global user.email bellothornus@gmail.com
-goto :final2
+goto :final
 
-:final2
+:final
 set account=
 echo se ha cambiado tu cuenta de Github 
